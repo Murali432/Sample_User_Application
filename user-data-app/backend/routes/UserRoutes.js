196d9719b60cb3d776f82efd900180a6e2c1d02f
Muralikrishna Y K <murali2001kichcha@gmail.com>
2024-09-20 12:13:43 +0530

    Initial commit

---
+++ user-data-app/backend/routes/UserRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const User = require("../models/Users");
+
+const router = express.Router();
+router.post("/add", async (req, res) => {
+  const { name, email, age } = req.body;
+  try {
+    const newUser = new User({ name, email, age });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+});
+
+// GET route to retrieve all users
+router.get("/Users", async (_req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error });
+  }
+});
+
+module.exports = router;
